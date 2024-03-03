013f0a035c560d90bb6e8ba67e5d6f1634b3e59e
motsuni <motsuni@yotta-rc.dev>
2024-03-03 23:43:45 +0900

    tasktray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,50 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::exit;
+
+use tauri::{AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
+fn handle_window(event: tauri::GlobalWindowEvent) {
+    match event.event() {
+        tauri::WindowEvent::CloseRequested { .. } => match event.window().label() {
+            "main" => {
+                exit(0);
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+}
+
+fn create_task_tray() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "終了");
+
+    let tray = SystemTrayMenu::new().add_item(quit);
+
+    let system_tray = SystemTray::new().with_menu(tray);
+
+    system_tray
+}
+
+fn handle_system_tray(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::LeftClick { .. } => {
+            let window = app.get_window("main").unwrap();
+
+            window.show().unwrap();
+            window.set_focus().unwrap();
+        }
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => {
+                exit(0);
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,6 +54,9 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(create_task_tray())
+        .on_window_event(handle_window)
+        .on_system_tray_event(handle_system_tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
