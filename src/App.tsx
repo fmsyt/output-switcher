aaabfa98d031b06268fdab6f017b813b6e3f701b
motsuni <motsuni@yotta-rc.dev>
2024-04-09 01:41:14 +0900

    control

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Box, Card, CardContent, CssBaseline, Stack, Typography } from "@mui/material";
+import { Box, Card, CardContent, CircularProgress, CssBaseline, IconButton, Slider, Stack, Typography } from "@mui/material";
+import { Suspense, useCallback, useMemo, useRef, useState } from "react";
 
 import ThemeProvider from "./ThemeProvider";
 import useWindowsAudioState from "./useWindowsAudioState";
 
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
 import VolumeOffIcon from '@mui/icons-material/VolumeOff';
-import { useMemo } from "react";
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import { invokeQuery } from "./ipc";
 
 function App() {
 
   const audioState = useWindowsAudioState();
+
+  const handlerIdRef = useRef<number | null>(null);
 
   const defaultDevice = useMemo(() => {
     if (!audioState) {
@@ -18,32 +21,85 @@ function App() {
     }
 
     return audioState.audioDeviceList.find(device => device.id === audioState.default);
-  }, [audioState]);
+  }, [audioState?.default]);
+
+  const initialVolume = useMemo(() => defaultDevice?.volume, [defaultDevice]);
+  const [muted, setMuted] = useState(defaultDevice?.muted);
+
+  const handleChangeVolume = useCallback((event: Event, volume: number | number[]) => {
+
+    if (!defaultDevice) {
+      return;
+    }
+
+    event.stopPropagation();
+    event.preventDefault();
+    console.log(volume);
+
+    if (handlerIdRef.current !== null) {
+      clearTimeout(handlerIdRef.current);
+    }
+
+    handlerIdRef.current = window.setTimeout(async () => {
+      await invokeQuery({
+        kind: "QVolumeChange",
+        id: defaultDevice.id,
+        volume: volume as number,
+      });
+    }, 50);
+  }, [defaultDevice])
+
+  const handleToggleMute = useCallback(async () => {
+
+    console.log(defaultDevice?.muted);
+    if (!defaultDevice) {
+      return;
+    }
+
+    setMuted(!muted);
+
+    await invokeQuery({
+      kind: "QMuteStateChange",
+      id: defaultDevice.id,
+      muted: !muted,
+    });
+
+  }, [defaultDevice, muted]);
 
   return (
     <ThemeProvider>
       <CssBaseline />
       <Box sx={{ width: "100%", height: "100vh" }}>
+
         <Card>
           <CardContent>
+            <Suspense fallback={<CircularProgress />}>
+              <Stack direction="row" alignItems="center" spacing={2}>
+                <IconButton onClick={handleToggleMute}>
+                  {muted ? <VolumeOffIcon /> : defaultDevice?.volume === 0 ? <VolumeMuteIcon /> : <VolumeUpIcon />}
+                </IconButton>
+                <Typography variant="h6">
+                  {defaultDevice?.name}
+                </Typography>
+                <Typography variant="h6">
+                  {defaultDevice?.volume}
+                </Typography>
+              </Stack>
 
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <Typography variant="h6">
-                {defaultDevice?.name}
-              </Typography>
-              <Typography variant="h6">
-                {defaultDevice?.volume}
-              </Typography>
-              <Typography variant="h6">
-                {defaultDevice?.muted ? "Muted" : "Not Muted"}
-              </Typography>
-              {defaultDevice?.muted ? <VolumeOffIcon /> : defaultDevice?.volume === 0 ? <VolumeMuteIcon /> : <VolumeUpIcon />}
-            </Stack>
-
+              <Stack direction="row" alignItems="center" spacing={2}>
+                <Slider
+                  defaultValue={initialVolume}
+                  onChange={handleChangeVolume}
+                  min={0}
+                  max={1}
+                  step={0.01}
+                />
+              </Stack>
+            </Suspense>
 
           </CardContent>
         </Card>
-        <pre>{JSON.stringify(audioState, null, 2)}</pre>
+
       </Box>
     </ThemeProvider>
   );
