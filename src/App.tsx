841e197b228d6c0c46445ea510cdaf002a0d7247
motsuni <motsuni@yotta-rc.dev>
2026-08-23 21:29:01 +0900

    スクロールバーの表示領域調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Box, CssBaseline, Stack } from "@mui/material";
 import { invoke } from "@tauri-apps/api/core";
-// import { LogicalSize } from "@tauri-apps/api/dpi";
 import { listen, type UnlistenFn } from "@tauri-apps/api/event";
 import { useEffect } from "react";
 import useConfig from "./config/useConfig";
@@ -71,16 +70,20 @@ function Container() {
         overflow: "hidden",
       }}
     >
-      <Box sx={{ flexShrink: 0 }}>
+      <Box sx={{
+        flexShrink: 0,
+        paddingRight: 1,
+      }}>
         <MasterVolumeControl device={defaultDevice} />
       </Box>
       <Box
         sx={{
           flex: 1,
           overflowY: "auto",
-          scrollbarWidth: "none",
-          ":hover": {
-            animation: "scrollbar-fade-in 0.2s ease-in-out forwards",
+          scrollbarWidth: "thin",
+          scrollbarGutter: "stable",
+          scrollbarColor: "rgba(255, 255, 255, 0.5) transparent",
+          "&:hover": {
           }
         }}
       >
