0a1e8491c27d757465e8f732e462bb3a528e8645
motsuni <motsuni@yotta-rc.dev>
2026-09-01 00:45:57 +0900

    refactor

--- src-tauri/src/ipc/init.rs
+++ src-tauri/src/ipc/init.rs
@@ -52,6 +52,246 @@ pub enum IPCHandlers {
 
 const RECEIVE_INTERVAL: Duration = Duration::from_millis(100);
 
+async fn spawn_relay_thread(
+    mut backend_update_rx: Receiver<Notification>,
+    ipc_tx: Sender<IPCHandlers>,
+) -> Result<()> {
+    while let Some(mut notification) = backend_update_rx.recv().await {
+        loop {
+            match timeout(RECEIVE_INTERVAL, backend_update_rx.recv()).await {
+                Ok(Some(n)) => notification = n,
+                _ => break,
+            }
+        }
+
+        ipc_tx
+            .send(IPCHandlers::AudioDictUpdate { notification })
+            .await
+            .map_err(|_| APIError::Unexpected {
+                inner: UnexpectedErr::MPSCClosedError,
+            })?;
+    }
+
+    Result::<()>::Ok(())
+}
+
+async fn spawn_backend_thread(
+    mut query_rx: Receiver<IPCHandlers>,
+    audio_dict: Arc<Mutex<AudioDeviceMap>>,
+    is: Arc<Singleton>,
+    frontend_update_tx: Sender<AudioStateChangePayload>,
+) -> Result<(), APIError> {
+    while let Some(q) = query_rx.recv().await {
+        #[cfg(debug_assertions)]
+        println!("Received IPC query: {:?}", q);
+
+        match q {
+            IPCHandlers::AudioDictUpdate { notification } => {
+                use super::audio::notifier::Notification;
+
+                let should_update_dict = matches!(
+                    notification,
+                    Notification::SessionCreated { .. } | Notification::SessionTerminated { .. }
+                );
+
+                if should_update_dict {
+                    let mut dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                        inner: UnexpectedErr::LockError,
+                    })?;
+                    *dict = get_audio_dictionary(&is).map_err(|e| APIError::SomethingWrong {
+                        msg: format!("@get_audio_dict {:?}", e),
+                    })?;
+                }
+
+                let e = ipc_sender(&is, &audio_dict, Some(notification), &frontend_update_tx).await;
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+            IPCHandlers::AudioDict => {
+                let e = ipc_sender(&is, &audio_dict, None, &frontend_update_tx).await;
+
+                if let Err(e) = e {
+                    log::error!("update_notifing_b2f {:?}", e);
+                }
+            }
+            IPCHandlers::DefaultAudioChange { id } => {
+                let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                    inner: UnexpectedErr::LockError,
+                })?;
+
+                let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
+                    msg: format!("No such audio: {:?}", id),
+                });
+
+                let audio = match audio_w {
+                    Ok(audio) => audio,
+                    Err(e) => {
+                        log::error!("{:?}", e);
+                        continue;
+                    }
+                };
+
+                let e = audio
+                    .set_as_default()
+                    .map_err(|e| APIError::SomethingWrong {
+                        msg: format!("audio.set_as_default {:?}", e),
+                    });
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+            IPCHandlers::VolumeChange { id, volume } => {
+                let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                    inner: UnexpectedErr::LockError,
+                })?;
+
+                let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
+                    msg: format!("No such audio: {:?}", id),
+                });
+
+                let audio = match audio_w {
+                    Ok(audio) => audio,
+                    Err(e) => {
+                        log::error!("{:?}", e);
+                        continue;
+                    }
+                };
+
+                let e = audio
+                    .set_volume(volume)
+                    .map_err(|e| APIError::SomethingWrong {
+                        msg: format!("@audio.set_volume {:?}", e),
+                    });
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+            IPCHandlers::MuteStateChange { id, muted } => {
+                let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                    inner: UnexpectedErr::LockError,
+                })?;
+
+                let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
+                    msg: format!("No such audio: {:?}", id),
+                });
+
+                let audio = match audio_w {
+                    Ok(audio) => audio,
+                    Err(e) => {
+                        log::error!("{:?}", e);
+                        continue;
+                    }
+                };
+
+                let e = audio
+                    .set_mute_state(muted)
+                    .map_err(|e| APIError::SomethingWrong {
+                        msg: format!("@audio.set_mute {:?}", e),
+                    });
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+            IPCHandlers::SessionVolumeChange {
+                id,
+                session_id,
+                volume,
+            } => {
+                let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                    inner: UnexpectedErr::LockError,
+                })?;
+
+                let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
+                    msg: format!("No such audio: {:?}", id),
+                });
+
+                let audio = match audio_w {
+                    Ok(audio) => audio,
+                    Err(e) => {
+                        log::error!("{:?}", e);
+                        continue;
+                    }
+                };
+
+                let e = audio.set_session_volume(&session_id, volume).map_err(|e| {
+                    APIError::SomethingWrong {
+                        msg: format!("@audio.set_session_volume {:?}", e),
+                    }
+                });
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+            IPCHandlers::SessionMuteStateChange {
+                id,
+                session_id,
+                muted,
+            } => {
+                let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                    inner: UnexpectedErr::LockError,
+                })?;
+
+                let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
+                    msg: format!("No such audio: {:?}", id),
+                });
+
+                let audio = match audio_w {
+                    Ok(audio) => audio,
+                    Err(e) => {
+                        log::error!("{:?}", e);
+                        continue;
+                    }
+                };
+
+                let e = audio
+                    .set_session_mute_state(&session_id, muted)
+                    .map_err(|e| APIError::SomethingWrong {
+                        msg: format!("@audio.set_session_mute_state {:?}", e),
+                    });
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+            IPCHandlers::AudioSessionDict { id } => {
+                let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
+                    inner: UnexpectedErr::LockError,
+                })?;
+
+                let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
+                    msg: format!("No such audio: {:?}", id),
+                });
+
+                let audio = match audio_w {
+                    Ok(audio) => audio,
+                    Err(e) => {
+                        log::error!("{:?}", e);
+                        continue;
+                    }
+                };
+
+                let e = audio
+                    .get_session_list()
+                    .map_err(|e| APIError::SomethingWrong {
+                        msg: format!("@audio.get_session_list {:?}", e),
+                    });
+
+                if let Err(e) = e {
+                    log::error!("{:?}", e);
+                }
+            }
+        }
+    }
+
+    Result::<(), APIError>::Ok(())
+}
+
 pub struct BackendPrepareRet {
     pub relay_thread: JoinHandle<Result<()>>,
     pub backend_thread: JoinHandle<Result<(), APIError>>,
@@ -66,25 +306,7 @@ pub async fn prepare_backend() -> Result<BackendPrepareRet> {
     let (frontend_update_tx, ipc_rx) = channel(256);
     let (ipc_tx, mut query_rx) = channel(256);
 
-    let qt = ipc_tx.clone();
-    let relay_thread = tokio::spawn(async move {
-        while let Some(mut notification) = backend_update_rx.recv().await {
-            loop {
-                match timeout(RECEIVE_INTERVAL, backend_update_rx.recv()).await {
-                    Ok(Some(n)) => notification = n,
-                    _ => break,
-                }
-            }
-
-            qt.send(IPCHandlers::AudioDictUpdate { notification })
-                .await
-                .map_err(|_| APIError::Unexpected {
-                    inner: UnexpectedErr::MPSCClosedError,
-                })?;
-        }
-
-        Result::<()>::Ok(())
-    });
+    let relay_thread = tokio::spawn(spawn_relay_thread(backend_update_rx, ipc_tx.clone()));
 
     let is =
         Arc::new(
@@ -102,226 +324,12 @@ pub async fn prepare_backend() -> Result<BackendPrepareRet> {
     let is_clone = is.clone();
     let audio_dict_clone = audio_dict.clone();
 
-    let backend_thread = tokio::spawn(async move {
-        while let Some(q) = query_rx.recv().await {
-            #[cfg(debug_assertions)]
-            println!("Received IPC query: {:?}", q);
-
-            match q {
-                IPCHandlers::AudioDictUpdate { notification } => {
-                    use super::audio::notifier::Notification;
-
-                    // SessionCreated または SessionTerminated の場合は辞書を更新
-                    let should_update_dict = matches!(
-                        notification,
-                        Notification::SessionCreated { .. }
-                            | Notification::SessionTerminated { .. }
-                    );
-
-                    if should_update_dict {
-                        let mut dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                            inner: UnexpectedErr::LockError,
-                        })?;
-                        *dict =
-                            get_audio_dictionary(&is).map_err(|e| APIError::SomethingWrong {
-                                msg: format!("@get_audio_dict {:?}", e),
-                            })?;
-                    }
-
-                    let e =
-                        ipc_sender(&is, &audio_dict, Some(notification), &frontend_update_tx).await;
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                        // continue;
-                    }
-                }
-                IPCHandlers::AudioDict => {
-                    let e = ipc_sender(&is, &audio_dict, None, &frontend_update_tx).await;
-
-                    if let Err(e) = e {
-                        log::error!("update_notifing_b2f {:?}", e);
-                        // continue;
-                    }
-                }
-                IPCHandlers::DefaultAudioChange { id } => {
-                    let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                        inner: UnexpectedErr::LockError,
-                    })?;
-
-                    let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
-                        msg: format!("No such audio: {:?}", id),
-                    });
-
-                    let audio = match audio_w {
-                        Ok(audio) => audio,
-                        Err(e) => {
-                            log::error!("{:?}", e);
-                            continue;
-                        }
-                    };
-
-                    let e = audio
-                        .set_as_default()
-                        .map_err(|e| APIError::SomethingWrong {
-                            msg: format!("audio.set_as_default {:?}", e),
-                        });
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                        // continue;
-                    }
-                }
-                IPCHandlers::VolumeChange { id, volume } => {
-                    let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                        inner: UnexpectedErr::LockError,
-                    })?;
-
-                    let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
-                        msg: format!("No such audio: {:?}", id),
-                    });
-
-                    let audio = match audio_w {
-                        Ok(audio) => audio,
-                        Err(e) => {
-                            log::error!("{:?}", e);
-                            continue;
-                        }
-                    };
-
-                    let e = audio
-                        .set_volume(volume)
-                        .map_err(|e| APIError::SomethingWrong {
-                            msg: format!("@audio.set_volume {:?}", e),
-                        });
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                        // continue;
-                    }
-                }
-                IPCHandlers::MuteStateChange { id, muted } => {
-                    let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                        inner: UnexpectedErr::LockError,
-                    })?;
-
-                    let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
-                        msg: format!("No such audio: {:?}", id),
-                    });
-
-                    let audio = match audio_w {
-                        Ok(audio) => audio,
-                        Err(e) => {
-                            log::error!("{:?}", e);
-                            continue;
-                        }
-                    };
-
-                    let e = audio
-                        .set_mute_state(muted)
-                        .map_err(|e| APIError::SomethingWrong {
-                            msg: format!("@audio.set_mute {:?}", e),
-                        });
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                        // continue;
-                    }
-                }
-                IPCHandlers::SessionVolumeChange {
-                    id,
-                    session_id,
-                    volume,
-                } => {
-                    let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                        inner: UnexpectedErr::LockError,
-                    })?;
-
-                    let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
-                        msg: format!("No such audio: {:?}", id),
-                    });
-
-                    let audio = match audio_w {
-                        Ok(audio) => audio,
-                        Err(e) => {
-                            log::error!("{:?}", e);
-                            continue;
-                        }
-                    };
-
-                    let e = audio.set_session_volume(&session_id, volume).map_err(|e| {
-                        APIError::SomethingWrong {
-                            msg: format!("@audio.set_session_volume {:?}", e),
-                        }
-                    });
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                    }
-                }
-                IPCHandlers::SessionMuteStateChange {
-                    id,
-                    session_id,
-                    muted,
-                } => {
-                    let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                        inner: UnexpectedErr::LockError,
-                    })?;
-
-                    let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
-                        msg: format!("No such audio: {:?}", id),
-                    });
-
-                    let audio = match audio_w {
-                        Ok(audio) => audio,
-                        Err(e) => {
-                            log::error!("{:?}", e);
-                            continue;
-                        }
-                    };
-
-                    let e = audio
-                        .set_session_mute_state(&session_id, muted)
-                        .map_err(|e| APIError::SomethingWrong {
-                            msg: format!("@audio.set_session_mute_state {:?}", e),
-                        });
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                    }
-                }
-                IPCHandlers::AudioSessionDict { id } => {
-                    let dict = audio_dict.lock().map_err(|_| APIError::Unexpected {
-                        inner: UnexpectedErr::LockError,
-                    })?;
-
-                    let audio_w = dict.get(&id).ok_or(APIError::SomethingWrong {
-                        msg: format!("No such audio: {:?}", id),
-                    });
-
-                    let audio = match audio_w {
-                        Ok(audio) => audio,
-                        Err(e) => {
-                            log::error!("{:?}", e);
-                            continue;
-                        }
-                    };
-
-                    let e = audio
-                        .get_session_list()
-                        .map_err(|e| APIError::SomethingWrong {
-                            msg: format!("@audio.get_session_list {:?}", e),
-                        });
-
-                    if let Err(e) = e {
-                        log::error!("{:?}", e);
-                    }
-                }
-            }
-        }
-
-        Result::<(), APIError>::Ok(())
-    });
+    let backend_thread = tokio::spawn(spawn_backend_thread(
+        query_rx,
+        audio_dict,
+        is,
+        frontend_update_tx,
+    ));
 
     Ok(BackendPrepareRet {
         relay_thread,
